7c99a95831b13824e82e09b96e72ea2b3eb98dcf
Salvatore Corvaglia <ingegnere.corvaglia@gmail.com>
2026-08-07 15:39:08 +0200

    chore: configure mocha settings and workspace folder for integration tests

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -2,4 +2,9 @@ import { defineConfig } from '@vscode/test-cli';
 
 export default defineConfig({
   files: 'out/test/integration/**/*.test.js',
+  mocha: {
+    ui: 'bdd',
+  },
+  workspaceFolder: 'test/fixtures',
 });
+
